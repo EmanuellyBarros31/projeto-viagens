2e8b39669bb1ba6c866db728a3fce51e5a3569a6
EmanuellyBarros31 <barrosemanuelly338@gmail.com>
2026-04-07 16:02:45 -0500

    atualizando

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Menu }from './menu/menu';
 import { Banner } from './banner/banner';
 import { SectionCenter } from './section-center/section-center';
 import { SectionPaises } from './section-paises/section-paises';
+import { Tours } from './tours/tours';
 import { Carrosel } from './carrosel/carrosel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu ,  Banner ,SectionCenter ,SectionPaises ,Carrosel] ,
+  imports: [RouterOutlet, Menu ,  Banner ,SectionCenter ,SectionPaises ,Tours,Carrosel] ,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
